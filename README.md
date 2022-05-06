0ba0e8adebc91c3853ae9cb944e72e7bcd8c736f
AndyMitschke <78310586+AndyMitschke@users.noreply.github.com>
2022-05-06 14:22:49 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,313 @@
 # Vector.ts
 A simple vector class written in TypeScript
+
+
+## Methods
+
+### .set(number, number)
+```Javascript
+const vec = new Vector(1, 2)
+vec.set(2, 2)
+// vec { x: 2, y: 2 }
+```
+
+### .copy()
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(3, 4)
+vec1.copy(vec2)
+// vec1 { x: 3, y: 4 }
+```
+
+### .up()
+```Javascript
+const vec = new Vector(1, 2)
+vec.up()
+// vec { x: 0, y: -1 }
+```
+
+### .down()
+```Javascript
+const vec = new Vector(1, 2)
+vec.down()
+// vec { x: 0, y: 1 }
+```
+
+### .left()
+```Javascript
+const vec = new Vector(1, 2)
+vec.left()
+// vec { x: -1, y: 0 }
+```
+
+### .right()
+```Javascript
+const vec = new Vector(1, 2)
+vec.right()
+// vec { x: 1, y: 0 }
+```
+
+### .add(Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(2, 3)
+vec1.add(vec2)
+// vec1 { x: 3, y: 5 }
+```
+
+### .subtract(Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(2, 3)
+vec1.subtract(vec2)
+// vec1 { x: -1, y: -1 }
+```
+
+### .scale(number)
+```Javascript
+const vec = new Vector(1, 2)
+vec.scale(2)
+// vec { x: 2, y: 4 }
+```
+
+### .divide(number)
+```Javascript
+const vec = new Vector(4, 6)
+vec.divide(2)
+// vec { x: 2, y: 3 }
+```
+
+### .normalize()
+```Javascript
+const vec = new Vector(1, 2)
+vec.normalize()
+// vec { x: 0.447..., y: 0.894... }
+```
+
+### .limit(number)
+```Javascript
+const vec = new Vector(1, 2)
+vec.limit(0.7)
+// vec { x: 0.313..., y: 0.626... }
+```
+
+### .zero()
+```Javascript
+const vec = new Vector(1, 2)
+vec.zero()
+// vec { x: 0, y: 0 }
+```
+
+### .flip()
+```Javascript
+const vec = new Vector(1, 2)
+vec.flip()
+// vec { x: -1, y: -2 }
+```
+
+### .flipHorizontal()
+```Javascript
+const vec = new Vector(1, 2)
+vec.flipHorizontal()
+// vec { x: 1, y: -2 }
+```
+
+### .flipVertical()
+```Javascript
+const vec = new Vector(1, 2)
+vec.flipVertical()
+// vec { x: -1, y: 2 }
+```
+
+### .rotate(number)
+```Javascript
+const vec = new Vector(0, 1)
+vec.rotate(Math.PI / 2)
+// vec { x: 1, y: 0 }
+```
+
+### .distanceTo(Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(2, 3)
+vec1.distanceTo(vec2)
+// 1.414...
+```
+
+### .angleTo(Vector)
+```Javascript
+const vec1 = new Vector(0, 1)
+const vec2 = new Vector(1, 0)
+vec1.angleTo(vec2)
+// 1.570...
+```
+
+### .lerp(target: Vector, steps: number, limit: number)
+```Javascript
+const vec1 = new Vector(0, 1)
+const vec2 = new Vector(1, 0)
+vec1.lerp(vec2, 0.1, 0.1)
+```
+
+### .moveTowards(target: Vector, steps: number, limit: number)
+```Javascript
+// not working yet
+const vec1 = new Vector(0, 1)
+const vec2 = new Vector(1, 0)
+vec1.moveTowards(vec2, 0.1, 0.1)
+```
+
+
+## Static Methods
+
+### Vector.createUnitVector(number)
+```Javascript
+const vec = Vector.createUnitVector(Math.PI / 2)
+// vec { x: 1, y: 0 }
+```
+
+### Vector.copy(Vector)
+```Javascript
+const vec1 = new Vector(2, 3)
+const vec2 = Vector.copy(vec1)
+// vec2 { x: 2, y: 3 }
+```
+
+### Vector.up()
+```Javascript
+const vec = Vector.up()
+// vec { x: 0, y: -1 }
+```
+
+### Vector.down()
+```Javascript
+const vec = Vector.down()
+// vec { x: 0, y: 1 }
+```
+
+### Vector.left()
+```Javascript
+const vec = Vector.left()
+// vec { x: -1, y: 0 }
+```
+
+### Vector.right()
+```Javascript
+const vec = Vector.right()
+// vec { x: 1, y: 0 }
+```
+
+### Vector.add(Vector, Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(3, 4)
+const result = Vector.add(vec1, vec2)
+// result { x: 4, y: 6 }
+```
+
+### Vector.subtract(Vector, Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(3, 4)
+const result = Vector.subtract(vec1, vec2)
+// result { x: -2, y: -2 }
+```
+
+### Vector.scale(Vector, number)
+```Javascript
+const vec = new Vector(1, 2)
+const sca = 3
+const result = Vector.scale(vec, sca)
+// result { x: 3, y: 6 }
+```
+
+### Vector.divide(Vector, number)
+```Javascript
+const vec = new Vector(4, 6)
+const sca = 2
+const result = Vector.divide(vec, sca)
+// result { x: 2, y: 3 }
+```
+
+### Vector.normalize(Vector)
+```Javascript
+const vec1 = new Vector(3, 4)
+const vec2 = Vector.normalize(vec1)
+// vec2 { x: 0.6, y: 0.8 }
+```
+
+### Vector.limit(Vector, number)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = Vector.limit(1.5)
+// vec2 { x: 0.6708, y: 1.3416 }
+```
+
+### Vector.rotate(Vector, number)
+```Javascript
+const vec1 = new Vector(0, 1)
+const vec2 = Vector.rotate(Math.PI / 2)
+// vec2 { x: 1, y: 0 }
+```
+
+### Vector.flip(Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = Vector.flip()
+// vec2 { x: -1, y: -2 }
+```
+
+### Vector.flipHorizontal(Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = Vector.flipHorizontal()
+// vec2 { x: 1, y: -2 }
+```
+
+### Vector.flipVertical(Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = Vector.flipVertical()
+// vec2 { x: -1, y: 2 }
+```
+
+### Vector.isEqual(Vector, Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(2, 1)
+const result = Vector.isEqual(vec1, vec2)
+// result false
+
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(1, 2)
+const result = Vector.isEqual(vec1, vec2)
+// result true
+```
+
+### Vector.distanceBetween(Vector, Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(3, 4)
+const result = Vector.distanceBetween(vec1, vec2)
+// result 1.414...
+```
+
+### Vector.angleBetween(Vector, Vector)
+```Javascript
+const vec1 = new Vector(0, 1)
+const vec2 = new Vector(1, 0)
+const result = Vector.angleBetween(vec1, vec2)
+// result 1.570...
+```
+
+### Vector.dotProduct(Vector, Vector)
+```Javascript
+const vec1 = new Vector(1, 2)
+const vec2 = new Vector(3, 4)
+const result = Vector.dotProduct(vec1, vec2)
+// result 11
+```
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/Vector.ts
@@ -0,0 +1,306 @@
+class Vector
+{
+  public x: number;
+  public y: number;
+
+  constructor(x = 0, y = 0) 
+  {
+    this.x = x;
+    this.y = y;
+  }
+
+  public get magnitude(): number
+  {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+
+  public set magnitude(magnitude: number)
+  {
+    this.normalize().scale(magnitude);
+  }
+
+  public get direction(): number
+  {
+    return Math.atan2(this.y, this.x);
+  }
+
+  public set direction(direction: number) 
+  {
+    var magnitude = this.magnitude;
+    const x = Math.round(10000 * (Math.sin(direction) * magnitude)) / 10000;
+    const y = Math.round(10000 * (Math.cos(direction) * magnitude)) / 10000;
+    this.x = x;
+    this.y = y;
+  }
+
+  public set(x: number, y: number): Vector
+  {
+    this.x = x;
+    this.y = y;
+    return this;
+  }
+
+  public copy(vector: Vector): Vector
+  {
+    this.x = vector.x;
+    this.y = vector.y;
+    return this;
+  }
+  
+  public up(): Vector
+  {
+    this.x = 0;
+    this.y = -1;
+    return this;
+  }
+
+  public down(): Vector
+  {
+    this.x = 0;
+    this.y = 1;
+    return this;
+  }
+
+  public left(): Vector
+  {
+    this.x = -1;
+    this.y = 0;
+    return this;
+  }
+
+  public right(): Vector
+  {
+    this.x = 1;
+    this.y = 0;
+    return this;
+  }
+
+  public add(vector: Vector): Vector
+  {
+    this.x += vector.x;
+    this.y += vector.y;
+    return this;
+  }
+
+  public subtract(vector: Vector): Vector
+  {
+    this.x -= vector.x;
+    this.y -= vector.y;
+    return this;
+  }
+
+  public scale(scalar: number): Vector
+  {
+    this.x *= scalar;
+    this.y *= scalar;
+    return this;
+  }
+
+  public divide(scalar: number): Vector
+  {
+    if(scalar === 0) return this;
+    this.x /= scalar;
+    this.y /= scalar;
+    return this;
+  }
+
+  public normalize(): Vector
+  {
+    const magnitude = this.magnitude;
+    if(magnitude === 0) return this;
+    this.x /= magnitude;
+    this.y /= magnitude;
+    return this;
+  }
+
+  public limit(limit: number): Vector
+  {
+    if(this.magnitude <= limit) return this;
+    return this.normalize().scale(limit);
+  }
+
+  public zero(): Vector
+  {
+    this.x = 0;
+    this.y = 0;
+    return this;
+  }
+
+  public flip(): Vector
+  {
+    this.x = -this.x;
+    this.y = -this.y;
+    return this;
+  }
+
+  public flipHorizontal(): Vector
+  {
+    this.y = -this.y;
+    return this;
+  }
+
+  public flipVertical(): Vector
+  {
+    this.x = -this.x;
+    return this;
+  }
+
+  public rotate(radians: number): Vector
+  {
+    const sin = Math.sin(radians);
+    const cos = Math.cos(radians);
+    const x = Math.round(10000 * (this.x * cos - this.y * sin)) / 10000;
+    const y = Math.round(10000 * (this.x * sin + this.y * cos)) / 10000;
+    this.x = x;
+    this.y = y;
+    return this;
+  }
+
+  public distanceTo(vector: Vector): number
+  {
+    const a = this.x - vector.x;
+    const b = this.y - vector.y;
+    return Math.sqrt(a ** 2 + b ** 2);
+  }
+
+  public angleTo(vector: Vector): number
+  {
+    return this.direction - vector.direction;
+  }
+
+  public lerp(target: Vector, amount: number = 0.2, limit: number = 0.1): Vector
+  {
+    if(Vector.distanceBetween(this, target) <= limit)
+    {
+      this.copy(target);
+      return this;
+    }
+    if(amount < 0) amount = 0;
+    if(amount > 1) amount = 1;
+    this.x += (target.x - this.x) * amount;
+    this.y += (target.y - this.y) * amount;
+    return this;
+  }
+
+  public moveTowards(target: Vector, amount: number = 0.2, limit: number = 0.1): Vector
+  {
+    // not working yet
+    return this;
+  }
+
+
+
+  public static up(): Vector
+  {
+    return new Vector().up();
+  }
+
+  public static down(): Vector
+  {
+    return new Vector().down();
+  }
+
+  public static left(): Vector
+  {
+    return new Vector().left();
+  }
+
+  public static right(): Vector
+  {
+    return new Vector().right();
+  }
+
+  public static createUnitVector(direction: number): Vector
+  {
+    const vector = new Vector(0, 1);
+    vector.direction = direction;
+    return vector;
+  }
+
+  public static copy(vector: Vector) 
+  {
+    return new Vector().copy(vector);
+  }
+
+  public static add(vectorA: Vector, vectorB: Vector): Vector
+  {
+    return Vector.copy(vectorA).add(vectorB);
+  }
+
+  public static subtract(vectorA: Vector, vectorB: Vector): Vector
+  {
+    return Vector.copy(vectorA).subtract(vectorB);
+  }
+
+  public static scale(vectorA: Vector, scalar: number): Vector
+  {
+    return Vector.copy(vectorA).scale(scalar);
+  }
+
+  public static divide(vectorA: Vector, scalar: number): Vector
+  {
+    return Vector.copy(vectorA).divide(scalar);
+  }
+
+  public static normalize(vector: Vector): Vector
+  {
+    return Vector.copy(vector).normalize();
+  }
+
+  public static limit(vector: Vector, limit: number): Vector
+  {
+    return Vector.copy(vector).limit(limit);
+  }
+
+  public static rotate(vector: Vector, radians: number): Vector
+  {
+    return Vector.copy(vector).rotate(radians);
+  }
+
+  public static flip(vector: Vector): Vector
+  {
+    return Vector.copy(vector).flip();
+  }
+
+  public static flipHorizontal(vector: Vector): Vector
+  {
+    return Vector.copy(vector).flipHorizontal();
+  }
+
+  public static flipVertical(vector: Vector): Vector
+  {
+    return Vector.copy(vector).flipVertical();
+  }
+
+  public static lerp(vector: Vector, target: Vector, amount: number = 0.2, limit: number = 0.1): Vector
+  {
+    return Vector.copy(vector).lerp(target, amount);
+  }
+
+  public static moveTowards(vector: Vector, target: Vector, amount: number = 0.2, limit: number = 0.1): Vector
+  {
+    return Vector.copy(vector).moveTowards(target, amount, limit);
+  }
+
+  public static isEqual(vectorA: Vector, vectorB: Vector): boolean
+  {
+    return vectorA.x === vectorB.x && vectorA.y === vectorB.y;
+  }
+
+  public static distanceBetween(vectorA: Vector, vectorB: Vector): number
+  {
+    return vectorA.distanceTo(vectorB);
+  }
+
+  public static angleBetween(vectorA: Vector, vectorB: Vector): number
+  {
+    return vectorA.angleTo(vectorB);
+  }
+
+  public static dotProduct(vectorA: Vector, vectorB: Vector): number
+  {
+    return vectorA.x * vectorB.x + vectorA.y * vectorB.y;
+  }
+}
+
+export { Vector };
